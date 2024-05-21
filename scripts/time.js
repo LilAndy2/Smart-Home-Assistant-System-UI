83573c5c67d8b8c8e3c3ef8fd1a75cf1536f3f3c
Andrei <iulian.andrei.manea@gmail.com>
2024-05-21 14:53:43 +0300

    added task scheduler

--- scripts/time.js
+++ scripts/time.js
@@ -2,27 +2,24 @@
 function updateLiveTime() {
     const currentTime = new Date();
     const hours = currentTime.getHours();
-    // Check if it's day or night
-    const isDay = hours >= 6 && hours < 18; // Assume day from 6 AM to 6 PM
-    const background = isDay ? 'day.jpg' : 'night.png';
+    // Background is always white
     const container = document.querySelector('.live-time-container');
-    container.style.backgroundImage = `url('images/${background}')`;
+    container.style.backgroundColor = 'white';
 
-    // Set text color to white if it's night
+    // Text color is always black
     const cityNames = document.querySelectorAll('.time-zone h3');
     cityNames.forEach(city => {
-        city.style.color = isDay ? 'black' : 'white';
+        city.style.color = 'black';
     });
 
-    // Set text color to black if it's day
     const otherText = document.querySelectorAll('.live-time-container h2, #live-time-clock, #live-day, #live-date');
     otherText.forEach(text => {
-        text.style.color = isDay ? 'black' : 'white';
+        text.style.color = 'black';
     });
 
     const timeZoneHours = document.querySelectorAll('.time-zone div');
     timeZoneHours.forEach(hour => {
-        hour.style.color = isDay ? 'black' : 'white';
+        hour.style.color = 'black';
     });
 
     const minutes = currentTime.getMinutes();
